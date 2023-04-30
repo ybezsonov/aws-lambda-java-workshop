6770b1e4128c976e7b8c182635593afd87fc1cc1
Yuriy Bezsonov <bezsonov@amazon.de>
2023-04-30 19:12:06 +0200

    decouple VPC in separate stack to deploy Cloud9 in it

--- labs/unicorn-store/setup-vpc.sh
+++ labs/unicorn-store/setup-vpc.sh
@@ -1,6 +1,14 @@
 #bin/sh
 
-# Deploy the VPC
+# Build the database setup function
+./mvnw clean package -f infrastructure/db-setup/pom.xml
+
+# Build the unicorn application
+./mvnw clean package -f software/alternatives/unicorn-store-basic/pom.xml
+./mvnw clean package -f software/unicorn-store-spring/pom.xml
+./mvnw clean package -f software/alternatives/unicorn-store-micronaut/pom.xml
+
+# Deploy the infrastructure
 cd infrastructure/cdk
 
 cdk bootstrap
