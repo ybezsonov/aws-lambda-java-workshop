268973e1c02b513411c28b6f49a108c2f0ed10da
Yuriy Bezsonov <bezsonov@amazon.de>
2023-05-08 11:29:09 +0200

    decouple VPC in separate stack to deploy Cloud9 in it

--- labs/unicorn-store/generate-vpc.sh
+++ labs/unicorn-store/generate-vpc.sh
@@ -2,13 +2,13 @@
 
 pushd infrastructure/cdk
 
-cdk synth UnicornStoreVpc --validation > ../../vpc.yml
+cdk synth UnicornStoreVpc --validation > ../../infrastructure/vpc.yml
 
 popd
 
-yq -i 'del(.Metadata)' vpc.yml
-yq -i 'del(.Parameters)' vpc.yml
-yq -i 'del(.Rules)' vpc.yml
-yq -i 'del(.Conditions)' vpc.yml 
-yq -i 'del(.Resources.CDKMetadata)' vpc.yml
-yq -i 'del(.Resources.*.Metadata)' vpc.yml
+yq -i 'del(.Metadata)' ./infrastructure/vpc.yml
+yq -i 'del(.Parameters)' ./infrastructure/vpc.yml
+yq -i 'del(.Rules)' ./infrastructure/vpc.yml
+yq -i 'del(.Conditions)' ./infrastructure/vpc.yml
+yq -i 'del(.Resources.CDKMetadata)' ./infrastructure/vpc.yml
+yq -i 'del(.Resources.*.Metadata)' ./infrastructure/vpc.yml
