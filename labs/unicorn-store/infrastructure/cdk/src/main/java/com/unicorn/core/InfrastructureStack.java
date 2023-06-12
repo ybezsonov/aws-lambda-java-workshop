f546009d36fa3aaeeb332f7de880e67f439dc1aa
Sascha Möllering <smoell@amazon.de>
2023-06-12 16:44:16 +0000

    Fixed RDS version and Dockerfile module dependency

--- labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/core/InfrastructureStack.java
+++ labs/unicorn-store/infrastructure/cdk/src/main/java/com/unicorn/core/InfrastructureStack.java
@@ -140,7 +140,7 @@ public class InfrastructureStack extends Stack {
     private DatabaseInstance createRDSPostgresInstance(IVpc vpc, DatabaseSecret databaseSecret) {
 
         var databaseSecurityGroup = createDatabaseSecurityGroup(vpc);
-        var engine = DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder().version(PostgresEngineVersion.VER_13_6).build());
+        var engine = DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder().version(PostgresEngineVersion.VER_13_7).build());
 
         return DatabaseInstance.Builder.create(this, "UnicornInstance")
                 .engine(engine)
