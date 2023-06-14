eb0a96c3ebc0a9f1f8357d0ad843af29a3a6558d
Yuriy Bezsonov <bezsonov@amazon.de>
2023-06-14 11:28:50 +0200

    Test cloud9

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -63,11 +63,11 @@ cd ~/environment/aws-java-workshop/labs/unicorn-store
 
 ## go to home directory
 cd ~/environment
-rm -vf ${HOME}/.aws/credentials
-export C9_ENV_ID=$(aws cloud9 describe-environments --environment-ids $(aws cloud9 list-environments --output json | jq -r '[.environmentIds[]] | join(" ")') --query "environments[?name == 'java-on-aws-workshop'].id" --output text)
-echo "Cloud9 environment ID: $C9_ENV_ID"
+# rm -vf ${HOME}/.aws/credentials
+# export C9_ENV_ID=$(aws cloud9 describe-environments --environment-ids $(aws cloud9 list-environments --output json | jq -r '[.environmentIds[]] | join(" ")') --query "environments[?name == 'java-on-aws-workshop'].id" --output text)
+# echo "Cloud9 environment ID: $C9_ENV_ID"
 
-aws cloud9 update-environment --environment-id $C9_ENV_ID --managed-credentials-action DISABLE
+# aws cloud9 update-environment --environment-id $C9_ENV_ID --managed-credentials-action DISABLE
 rm -vf ${HOME}/.aws/credentials
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
