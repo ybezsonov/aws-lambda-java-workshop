bff0d9a63112c1f9e3574015d0d9cb1269f1bb64
Yuriy Bezsonov <bezsonov@amazon.de>
2023-06-14 14:49:30 +0200

    Test cloud9

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -68,12 +68,17 @@ cd ~/environment
 # echo "Cloud9 environment ID: $C9_ENV_ID"
 
 # aws cloud9 update-environment --environment-id $C9_ENV_ID --managed-credentials-action DISABLE
-rm -vf ${HOME}/.aws/credentials
-export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
-echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
-aws configure set default.region ${AWS_REGION}
-aws configure get default.region
-test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
-aws sts get-caller-identity --query Arn | grep java-on-aws-workshop-admin -q && echo "IAM role is valid" || echo "IAM role is NOT valid"
+# rm -vf ${HOME}/.aws/credentials
+
+echo "Cloud9 environment ID: $C9_PID"
+aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE
+# rm -vf ${HOME}/.aws/credentials
+
+# export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+# export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+# echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+# echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+# aws configure set default.region ${AWS_REGION}
+# aws configure get default.region
+# test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
+# aws sts get-caller-identity --query Arn | grep java-on-aws-workshop-admin -q && echo "IAM role is valid" || echo "IAM role is NOT valid"
